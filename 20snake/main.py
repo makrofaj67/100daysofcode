5ceb148f853666fc1058a4bd67e97da8c0cabe3b
makrofaj67 <114802232+makrofaj67@users.noreply.github.com>
2023-04-13 18:11:42 +0300

    Update main.py
    #adem günesen request

--- 20snake/main.py
+++ 20snake/main.py
@@ -8,6 +8,12 @@ ekran.title("yılan oyunu")
 ekran.tracer(0)
 my_snake = snake.Snake()
 
+ekran.listen()
+ekran.listen(my_snake.up, "Up")
+ekran.listen(my_snake.down, "Down")
+ekran.listen(my_snake.left, "Left")
+ekran.listen(my_snake.right, "Right")
+
 game_is_on = True
 while game_is_on:
  ekran.update()
