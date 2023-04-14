0095f16a2ac5955dde4c15f02a3d63b61d94a205
makrofaj67 <114802232+makrofaj67@users.noreply.github.com>
2023-04-14 10:42:57 +0300

    Update scoreboard.py

--- 20snake/scoreboard.py
+++ 20snake/scoreboard.py
@@ -8,9 +8,15 @@ class Scoreboard(turtle.Turtle):
   self.color("white")
   self.ht()
   self.score = 0
-  self.write(f"Score: {self.score}", align="center", font=("Courier", 24, "normal"))
   
  def puanart(self):
   self.score += 1
   self.clear()
   self.write(f"Score: {self.score}", align="center", font=("Courier", 24, "normal"))
+
+ def puanbaslangıc(self):
+  self.write(f"Score: {self.score}", align="center", font=("Courier", 24, "normal"))
+  
+ def gameover(self):
+  self.goto(0, 0)
+  self.write(f"Game Over, Score: {self.score}", align="center", font=("Courier", 24, "normal"))
